047e6750669d3119392fc8af98c55d00203f53bd
Chen Yu <scheduledx@gmail.com>
2017-12-08 14:07:31 +0800

    collbab fix

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+    public Transform target;
+
+    // For orbit cam
+    [Header("Orbit Camera Settings")]
+    public float orbitSpeed = 360;
+    public float orbitDistance = -0;
+    public Vector3 orbitTargetOffset;
+    public Vector3 orbitCameraOffset;
+    public float orbitYLowestAngle = -45;
+    public float orbitYHighestAngle = 45;
+    private Vector2 orbitCurrentOrbit = new Vector2();
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 finalTargetPosition = target.position + orbitTargetOffset;
+        // Calculate orbit
+        orbitCurrentOrbit.x += Input.GetAxis("Mouse X") * Time.deltaTime * orbitSpeed;
+        orbitCurrentOrbit.y += Input.GetAxis("Mouse Y") * Time.deltaTime * orbitSpeed;
+        orbitCurrentOrbit.y = Mathf.Clamp(orbitCurrentOrbit.y, orbitYLowestAngle, orbitYHighestAngle);
+        // Calculate offset caused by orbit
+        Vector3 currentOffset = Quaternion.Euler(orbitCurrentOrbit.y, orbitCurrentOrbit.x, 0) * new Vector3(0, 0, orbitDistance);
+        // Change position to orbited position
+        transform.position = finalTargetPosition + currentOffset;
+        // Look at target
+        transform.LookAt(finalTargetPosition);
+        // Positionally offset, basically look to the side etc
+        transform.Translate(orbitCameraOffset);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float MovementSpeed = 1;
+    public float turnspeed = 10;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            GetComponent<CharacterController>().Move(new Vector3(0, 0, MovementSpeed));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            GetComponent<CharacterController>().Move(new Vector3(0, 0, -MovementSpeed));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, -MovementSpeed * turnspeed, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, MovementSpeed * turnspeed, 0);
+        }
+    }
+}
