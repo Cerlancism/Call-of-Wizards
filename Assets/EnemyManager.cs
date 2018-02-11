9b66e8e0f309f3df3ffff0a932c77848e9f36cf5
Chen Yu <scheduledx@gmail.com>
2018-02-11 22:40:46 +0800

    Floor done

--- Assets/EnemyManager.cs
+++ Assets/EnemyManager.cs
@@ -3,9 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyManager : MonoBehaviour {
+    public AudioSource combatMusicSource;
     public AudioClip combatMusicBegin;
     public AudioClip combatMusicLoop;
     public AudioClip combatMusicEnd;
 
-    //public 
+    private List<Enemy> enemies;
+
+    public void RegisterEnemy(Enemy enemy)
+    {
+        enemies.Add(enemy);
+    }
+
+    private void Update()
+    {
+        
+    }
 }
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,4 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour {
+    [Header("Enemy Settings")]
+    public EnemyManager enemyManager;
+
+    private void Start()
+    {
+        enemyManager.RegisterEnemy(this);
+    }
 }
